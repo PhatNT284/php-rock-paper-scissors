4b39338ed26c1d3d813a7ea2abafe4aae66643ae
PhatNT284 <phat280405@gmail.com>
2025-05-22 11:10:51 +0700

    Update ind.php

--- ind.php
+++ ind.php
@@ -28,7 +28,7 @@ if ( isset($_POST['who']) && isset($_POST['pass']) ) {
 <html>
 <head>
 <?php require_once "bootstrap.php"; ?>
-<title>Dang Huong ed30e57d</title>
+<title>Nghe Tai Phat 496fc56d</title>
 </head>
 <body>
 <div class="container">
